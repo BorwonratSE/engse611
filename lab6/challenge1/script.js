b16bef23f34e9a9ab3b867780f9c95e990f0f855
BorwonratSE <borwonrat_si67@live.rmutl.ac.th>
2025-03-16 23:32:45 +0700

    LAB6

---
+++ lab6/challenge1/script.js
@@ -0,0 +1,3 @@
+let element = document.getElementsByTagName("main");
+element.style.frontSize ="24px";
+element.style.color ="red";
---
+++ lab6/challenge2/script.js
@@ -0,0 +1,2 @@
+let element = document.querySelectorAll("#main p");
+element[5].innerHTML = "<span style ='color: red;' > Llamas and Chickens! </span> "
